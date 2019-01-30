e10f1f88631b9d23293da3b2b83fdf9a006e7487
Carina Cruz <lecruz@bu.edu>
2019-01-30 17:06:00 +0000

    setclass.js

---
+++ setClass.js
@@ -0,0 +1,14 @@
+function Set() {
+    // the var collection will hold our set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // change code below this line
+    // change code above this line
+}
