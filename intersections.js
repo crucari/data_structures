2c5b5eaadb05e1fa37b87cb2db3783cf3af51e40
Carina Cruz <lecruz@bu.edu>
2019-01-31 23:42:47 +0000

    intersection.sj

---
+++ intersections.js
@@ -0,0 +1,7 @@
+function Set() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
